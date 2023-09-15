a8e392dfd3226f93f28d1e3a0dd47e4201b690dc
b0bac <1914518025@qq.com>
2023-09-15 10:42:23 +0800

    support https

--- ApolloScanner/settings.py
+++ ApolloScanner/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_crontab',
+    'django_extensions',
+    'werkzeug_debugger_runserver',
 ]
 
 MIDDLEWARE = [
